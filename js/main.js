408babd4e65511eff10c78c80a4e6a935b4ad35f
Guilherme Vieira <super.driver.512@gmail.com>
2015-05-19 23:48:00 -0300

    WIP.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,71 @@
 document.addEventListener('DOMContentLoaded', function setupEditors() {
-	$('.editor').each(function(i, editorContainer) {
-		var $editorContainer = $(editorContainer);
-		var editor = ace.edit(editorContainer);
-		var theme = editorContainer.getAttribute('theme') || 'monokai';
-		var mode = editorContainer.getAttribute('mode') || 'javascript';
+	$('.editor').each(function(i, editor) {
+		var $editor = $(editor);
+		var aceEditor = ace.edit(editor);
+		var components = findClosestEditorComponents($editor);
+		var originalEditorContents = aceEditor.getValue();
+		var theme = editor.getAttribute('theme') || 'monokai';
+		var mode = editor.getAttribute('mode') || 'javascript';
+		aceEditor.setOptions({ maxLines: 30 });
 		if(theme) {
-			editor.setTheme('ace/theme/' + theme);
+			aceEditor.setTheme('ace/theme/' + theme);
 		}
 		if(mode) {
-			editor.getSession().setMode('ace/mode/' + mode);
+			aceEditor.getSession().setMode('ace/mode/' + mode);
 		}
-		$editorContainer.data('aceEditor', editor);
-		$editorContainer.data('originalContents', editor.getValue());
+		$editor.data('aceEditor', aceEditor);
+		$editor.data('originalContents', originalEditorContents);
+		if(originalEditorContents !== '') {
+			components.$runButton.click();
+		}
+		components.$outputContainer.data (
+			'originalContents',
+			components.$outputContainer.text()
+		);
 	});
+});
+function findClosestEditorComponents($element) {
+	var components = {};
+	components.$root = $element.closest('.editor-area');
+	components.$runButton = components.$root.find('.run-button');
+	components.$resetButton = components.$root.find('.reset-button');
+	components.$editor = components.$root.find('.editor');
+	components.aceEditor = components.$editor.data('aceEditor');
+	components.$outputContainer = components.$root.find('.program-output');
+	return components;
+}
+$(document).on('click', '.run-button', function() {
+	var $this = $(this);
+	var components = findClosestEditorComponents($this);
+	components.$outputContainer.text('');
+	try {
+		(new Function (
+			'console',
+			"'use strict';\n" + components.aceEditor.getValue()
+		))({
+			log: function() {
+				components.$outputContainer.text (
+					components.$outputContainer.text()
+					+ [].join.call(arguments, ' ') + '\n'
+				);
+			},
+			error: console.error.bind(console),
+		});
+	}
+	catch(error) {
+		components.$outputContainer.text (
+			"Erro na linha " + (error.lineNumber - 1) + "!"
+			+ '\n\n' + error
+		);
+	}
 });
 $(document).on('click', '.reset-button', function() {
 	var $this = $(this);
-	var editor = (
-		$this.closest('.editor-controls')
-			.siblings('.editor')
-			.data('aceEditor')
+	var components = findClosestEditorComponents($this);
+	components.aceEditor.setValue (
+		components.$editor.data('originalContents')
 	);
-	editor.setValue($this.data('originalContents'));
+	components.$outputContainer.text (
+		components.$outputContainer.data('originalContents')
+	);
 });
